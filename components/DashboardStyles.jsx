1a0374af2ee95772208aa570a40a08d9aa6ac019
enescedev <selimenesc@gmail.com>
2024-01-02 21:23:19 +0300

    Update dashboard styles~

---
+++ components/DashboardStyles.jsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/DashboardStyles.module.css';
+
+export default function DashboardStyles() {
+  return (
+    <div className={styles.dashboard}>
+      {/* İhtiyaç duyduğunuz diğer bileşenler burada olabilir */}
+    </div>
+  );
+}
